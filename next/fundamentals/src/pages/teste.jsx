f90c384a2399ee3789ff642859b756a638359722
LucasBraida <lucbnaza@gmail.com>
2022-02-11 11:37:54 -0300

    next tutorial alterations

---
+++ next/fundamentals/src/pages/teste.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+export default function Teste(){
+    return(
+        <div>
+            <Link href="/">
+                <button>Olha o teste</button>
+            </Link>
+
+        </div>
+    )
+}
