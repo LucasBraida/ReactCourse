0d7e8777b20e63924b98c027816c9e8726637ddd
LucasBraida <lucbnaza@gmail.com>
2022-02-14 16:40:08 -0300

    Backend Api

--- toDo-app/backend/src/config/routes.js
+++ toDo-app/backend/src/config/routes.js
@@ -4,5 +4,10 @@ const express = require("express")
 module.exports = function(server) {
 
     const router = express.Router()
+    const todoService = require("../api/todo/todoService")
+
     server.use("/api", router)
+    todoService.register(router, "/todos")
+
+
 }
--- toDo-app/backend/src/loader.js
+++ toDo-app/backend/src/loader.js
@@ -1,2 +1,5 @@
-require("./config/server")
+const server = require("./config/server")
 require("./config/database")
+const routes = require("./config/routes")
+routes(server)
+
