0b7b190c1b6046fe69d6a91cafb75781b3833dab
LucasBraida <lucbnaza@gmail.com>
2022-03-04 10:34:29 -0300

    Backdrop added to Pop-up component

--- react-pop-up/src/App.js
+++ react-pop-up/src/App.js
@@ -3,6 +3,13 @@ import Popup from './Popup';
 import DeleteIcon from '@mui/icons-material/Delete';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
+import BasicModal from './BasicModal';
+
+function Backdrop(props){
+  return (
+    props.show ? <div className='backdrop' onClick={props.clicked}></div> : null
+  )
+}
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,6 +17,8 @@ function App() {
   const togglePopup = () => {
     setIsOpen(!isOpen);
   }
+
+
 
   return <div>
     <input
@@ -22,19 +31,22 @@ function App() {
       togglePopup()}}
     />
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    {isOpen && <Popup
+    <Popup
       content={<>
         <b>Design your Popup</b>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <button>Test button</button>
       </>}
       handleClose={togglePopup}
-    />}
+      show={isOpen}
+    />
     <Tooltip title="Delete">
       <IconButton>
         <DeleteIcon />
       </IconButton>
     </Tooltip>
+    <BasicModal />
+
   </div>
 }
 
--- react-pop-up/src/Popup.jsx
+++ react-pop-up/src/Popup.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 
+function Backdrop(props){
+  return (
+    props.show ? <div className='backdrop' onClick={props.clicked}>{props.children}</div> : null
+  )
+}
 const Popup = props => {
   return (
-    <div className="popup-box">
+    <Backdrop show={props.show} clicked={props.handleClose}>
+      <div className="popup-box" onClick={props.handleClose}>
       <div className="box">
         <span className="close-icon" onClick={props.handleClose}>s2</span>
         {props.content}
       </div>
     </div>
+    </Backdrop>
+
   );
 };
 
